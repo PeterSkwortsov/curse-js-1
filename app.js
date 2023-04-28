774bc976df5b6626e3d5d766be0e44e3008ffdf6
Peter <skwortsow.petia@yandex.ru>
2023-04-28 14:58:23 +0300

    Задача

--- app.js
+++ app.js
@@ -1,25 +1,56 @@
-// Пользователь -
+/*
+Есть выгрузка операций пользователя
+const operations = [1000, -700, 300, -500, 10000];
+а так же начальный баланс в 100$
+Необходимо сделать функции расчета:
+- Итогового баланса
+- Наличия отрицательного баланса (если после очередной операции баланс < 0, то выдавать false)
+- Расчета среднего расхода и среднего дохода
+*/
 
-// Возраст
-// Наличие работы
-// Деньги
+const operation = [1000, -700, 300, -500, 10000];
+const startMoney = 100;
 
-// Нужно проверить может ли он купить новый MacBook за 2000$? Он может брать не только свои деньги, но и взять кредит. Ему дадут 500$, только если ему больше 24-х лет и он имеет работу, 100$ если ему просто больше 24-х лет и 0 в ином случае. Напишите функцию, которая принимает данные пользователя и товара и возвращает true или false.
+function getBalance(arrayOfOperations, initalibalance) {
+    let balance = initalibalance;
+    for (const element of arrayOfOperations) {
+            balance += element; 
+    }
+    return balance
+}
+console.log(getBalance(operation, startMoney))
 
-function computerCredit(age, hasJob = false) {
-    switch(true) {
-    case age > 24 && hasJob == true:
-        return 500
-    case age > 24:
-        return 100
-        default: 
-        return 0
+function chekoperations(arrayOfOperations, initalibalance) {
+    let balance = initalibalance;
+    let isOk = true;
+    for (const element of arrayOfOperations) {
+            balance += element; 
+            if (balance < 0) {
+                isOk = false;
+                break;
+            }
+        return isOk;
     }
 }
 
-function canBuy(productPrice, age, money, hasJob = false) {
-    const creditMoney = computerCredit(age, hasJob)
-    return productPrice <= money + creditMoney
+console.log(chekoperations(operation, startMoney))
+
+function avarigeOperations(arrayOfOperations) {
+    let positivCount = 0;
+    let negativCount = 0;
+    let posityvSumm = 0;
+    let negativeSumm = 0;
+    for (const element of arrayOfOperations) {
+        if (element > 0) {
+            positivCount++;
+            posityvSumm += element;
+        }
+        if (element < 0) {
+            negativCount++;
+            negativeSumm += element;
+        }
+    }
+    return [posityvSumm / positivCount, negativeSumm / negativCount];
 }
 
-console.log(canBuy(2000, 25, 1500, true))
+console.log(avarigeOperations(operation))
