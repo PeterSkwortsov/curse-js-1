def49c1d3943b294505730a9b286d1baf97801a3
Peter <skwortsow.petia@yandex.ru>
2023-05-02 12:46:05 +0300

    Список с индексои и номером

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
 
-function power (pow) {
-    return function (num) {
-        return num**pow;
-    }
+const score = [5, 10, 0, 15];
+for (const [i, el] of score.entries()) {
+    console.log(`Раунд: ${i+1}: ${el}`)
 }
-
-const powerOfTwo = power(2);
-console.log(powerOfTwo(5))
-console.log(powerOfTwo(10))
-
-const powerOfThree = power(3);
-console.log(powerOfThree(10))
-
-console.log(`Пока не понимаю - ${power(5)(4)}`)
