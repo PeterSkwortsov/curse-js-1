2eaa523fdcfcd5fda7df7fc9006b02e772dad869
Peter <skwortsow.petia@yandex.ru>
2023-05-02 11:54:32 +0300

    Колбек функция

--- app.js
+++ app.js
@@ -1,56 +1,28 @@
-/*
-Есть выгрузка операций пользователя
-const operations = [1000, -700, 300, -500, 10000];
-а так же начальный баланс в 100$
-Необходимо сделать функции расчета:
-- Итогового баланса
-- Наличия отрицательного баланса (если после очередной операции баланс < 0, то выдавать false)
-- Расчета среднего расхода и среднего дохода
-*/
-
-const operation = [1000, -700, 300, -500, 10000];
-const startMoney = 100;
-
-function getBalance(arrayOfOperations, initalibalance) {
-    let balance = initalibalance;
-    for (const element of arrayOfOperations) {
-            balance += element; 
-    }
-    return balance
-}
-console.log(getBalance(operation, startMoney))
-
-function chekoperations(arrayOfOperations, initalibalance) {
-    let balance = initalibalance;
-    let isOk = true;
-    for (const element of arrayOfOperations) {
-            balance += element; 
-            if (balance < 0) {
-                isOk = false;
-                break;
-            }
-        return isOk;
-    }
+function add (a, b) {
+return a + b 
 }
 
-console.log(chekoperations(operation, startMoney))
-
-function avarigeOperations(arrayOfOperations) {
-    let positivCount = 0;
-    let negativCount = 0;
-    let posityvSumm = 0;
-    let negativeSumm = 0;
-    for (const element of arrayOfOperations) {
-        if (element > 0) {
-            positivCount++;
-            posityvSumm += element;
-        }
-        if (element < 0) {
-            negativCount++;
-            negativeSumm += element;
-        }
-    }
-    return [posityvSumm / positivCount, negativeSumm / negativCount];
+function subtract (a, b) {
+    return a - b
 }
 
-console.log(avarigeOperations(operation))
+function power (a, b) {
+    return a **5
+}
+
+// функция высшего порядка
+
+function calculator (a, b, fn) {
+    const res = fn(a, b);
+    console.log(fn.name) 
+    return res
+    }
+
+    let res = calculator (2, 5, add);
+    console.log(res)
+
+    res = calculator (2, 5, subtract);
+    console.log(res)
+
+    res = calculator (2, 5, power);
+    console.log(res)
