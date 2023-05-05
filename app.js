f99ab9be680fd8523b0667c488c7bc4e389c9159
Peter <skwortsow.petia@yandex.ru>
2023-05-05 11:48:03 +0300

    Метод reduce

--- app.js
+++ app.js
@@ -1,44 +1,25 @@
-/*
-Имеется массив изменения цен prices, где внутри
-1й элемент массива является ценой в момент Х,
-2й - ценой в момент Y.
-Нужно преобразовать данные в массив, где будут отображены
-только положительные изменения цен: [100, 150]
-*/
-const prices = [[100,200], [120,100], [200,350]];
 
-const resuit = prices
-    .map(product => product[1] - product[0])
-    .filter(price => price > 0);
-        console.log(resuit)
+//тут мы складывем все значения массива
+
+const operationS = [100, -20, 7, -30, 50];
+
+const finalBalance1 = operationS.reduce((acc, operation, i) => {
+    console.log(`Итерация: ${i}, acc: ${acc}, операция: ${operation}`)
+    return acc += operation
+}, 0);
+
+console.log(finalBalance1)
 
 
+//тут мы находим наименьшее значение массива
 
-// /*Добавляем элементы в массив */
+const finalBalance2 = operationS.reduce((acc, operation, i) => {
+    console.log(`Итерация: ${i}, acc: ${acc}, операция: ${operation}`)
+    if (operation > acc) {
+        return acc;
+     } else {
+        return operation }
+    
+}, 0);
 
-// const operationS = [12, -5, 44, -23, 34, 2];
-// const positiveOperations = [];
-//     for(let operation of operationS) {
-//         if (operation > 0) {
-//             positiveOperations.push(operation);
-//         }
-//     }
-//     console.log(positiveOperations)
-
-
-// /*Фильтруем элименты через метод - фильтр */
-
-// const positiveOperationsS = operationS.filter(operation => {
-//     return operation < 1
-// })
-// console.log(positiveOperationsS)
-
-
-// /*Фильтруем и производим действие с элементом */
-
-// const positiveRUBOperations = operationS
-//     .filter(operation => {
-//     return operation > 0
-//     })
-//     .map(operation => operation * 60)
-// console.log(positiveRUBOperations)
+console.log(finalBalance2)
