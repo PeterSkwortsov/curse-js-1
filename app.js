04239f57722986b64635eefbf1c221e1fbf10438
Peter <skwortsow.petia@yandex.ru>
2023-05-05 14:39:10 +0300

    Сортировка массива методом Sort()

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
 
-
-const arr = [1, 4, 4, 10]
-    
-function some(arr, element) {
-    const res = arr.find( el => el === element) 
-        return res == undefined ? false : true
-    }
-console.log(some(arr, 10))
-
-// или 
-
-console.log(arr.some(el => el === 10))
-
+const price = [-3, 4, 34, 20, -10];
+price.sort((a,b) => a - b)
+console.log(price)
