5df16bdf2928fdab02c6d47006a07f6acf4713e7
Peter <skwortsow.petia@yandex.ru>
2023-05-10 13:02:14 +0300

    Упражнение кошеле с методами (не понял как???)

--- app.js
+++ app.js
@@ -1,26 +1,44 @@
-/* Преобразовать пользователя до вида 
-{ fullName: 'Вася Пупкин', skillNum: 2 }
+/* Реализовать методы увеличения и уменьшения баланса,
+при котором каждая операция сохраняется в массив
+operations в виде { reason: 'Оплата налогов', sum: -100 }.
+Возвращается true, если успешно и false, если не зватает баланса.
+Также реализовать метод вывода числа операций по кошельку
 */
-const users = [
-{
-    name: 'Вася',
-    surname: 'Пупкин',
-    age: 30,
-    skills: ['Разработка', 'Dev0ps']
-},
-{
-    name: 'Катя',
-    surname: 'Белова',
-    age: 18,
-    skills: ['Дизайн']
-},
-]
 
-const result = users.map(stroke => {
-    return {
-        fullName: `${stroke.name} ${stroke.surname}`,  
-        skillNum: stroke.skills.length
+const wallet = {
+balance: 0,
+operations: [],
+increase: function (reason, sum) {
+    this.balance += sum;
+    this.operations.push({
+        reason: reason,
+        sum: sum
+    })
+    return true
+},
+decrease: function (reason, sum) {
+    if (this.balance < sum) {
+        console.log('недостаточно баланса');
+        this.operations.push({
+            reason: reason,
+            sum: -sum
+        })
+        return false;
     }
-})
+    this.balance -= sum;
+    return true
+    },
 
-console.log(result)
+    getOperationLenhth: function () {
+        return this.operations.length;
+    }
+};
+
+console.log(wallet.increase('Зарплата', 1000))
+console.log(wallet.getOperationLenhth())
+console.log(wallet.decrease('Зарплата', 2400))
+console.log(wallet.getOperationLenhth())
+console.log(wallet.decrease('Зарплата', 500))
+console.log(wallet.balance)
+console.log(wallet.operations)
+
