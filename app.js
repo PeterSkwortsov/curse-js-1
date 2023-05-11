996f9c1fa0837e9ea7a2c8c0b14545a6585ae19f
Peter <skwortsow.petia@yandex.ru>
2023-05-11 11:02:32 +0300

    Итерирование по объекту не до конца понял

--- app.js
+++ app.js
@@ -1,44 +1,20 @@
-/* Реализовать методы увеличения и уменьшения баланса,
-при котором каждая операция сохраняется в массив
-operations в виде { reason: 'Оплата налогов', sum: -100 }.
-Возвращается true, если успешно и false, если не зватает баланса.
-Также реализовать метод вывода числа операций по кошельку
-*/
 
-const wallet = {
-balance: 0,
-operations: [],
-increase: function (reason, sum) {
-    this.balance += sum;
-    this.operations.push({
-        reason: reason,
-        sum: sum
-    })
-    return true
-},
-decrease: function (reason, sum) {
-    if (this.balance < sum) {
-        console.log('недостаточно баланса');
-        this.operations.push({
-            reason: reason,
-            sum: -sum
-        })
-        return false;
-    }
-    this.balance -= sum;
-    return true
+const sites = {
+    msk: {
+        let: 200,
+        temp: 50,
     },
-
-    getOperationLenhth: function () {
-        return this.operations.length;
+    spb: {
+        it: 100,
+        temp: 5,
     }
-};
+}
 
-console.log(wallet.increase('Зарплата', 1000))
-console.log(wallet.getOperationLenhth())
-console.log(wallet.decrease('Зарплата', 2400))
-console.log(wallet.getOperationLenhth())
-console.log(wallet.decrease('Зарплата', 500))
-console.log(wallet.balance)
-console.log(wallet.operations)
+let sumTemp = 0;
+let citesCount = Object.keys(sites).length
 
+for (const key of Object.keys(sites)) {
+    sumTemp += sites[key].temp;
+}
+console.log(sumTemp)
+console.log(sumTemp / citesCount)
