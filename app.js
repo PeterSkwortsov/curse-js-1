3badb9cfacfd4ef5929f31a3c1ffe977de7aae13
Peter <skwortsow.petia@yandex.ru>
2023-05-16 17:06:27 +0300

    Делю сумму первой половины массива на сумму элементов второго массива

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 'use strict'
-/* Дан массив с числами. Подсчитайте количество отрицательных чисел в этом массиве.
-
+/* Дан некоторый массив, например, вот такой:
+[1, 2, 3, 4, 5, 6]
+Поделите сумму первой половины элементов этого массива на сумму второй половины элементов.
  */
 
 
-let arr = [-1 ,-2 ,-3 ,-4 ,-5 , 1 ,  2 , 3 , 4 , 5];
-let res = arr.filter((el) => el < 0).length;
-console.log(res)
+let massive = [1, 2, 3, 4, 5, 6];
+let result1 = 0;
+let result2 = 0;
+
+let mas1 = massive.slice(0,3)
+    result1 = mas1.reduce(function(sum, elem) {
+	return sum + elem;
+}, 0);
+
+console.log(result1);
+
+let mas2 = massive.slice(3,6)
+    result2 = mas2.reduce(function(sum, elem) {
+	return sum + elem;
+}, 0);
+console.log(result2);
+
+console.log(Math.round(result2 / result1));
