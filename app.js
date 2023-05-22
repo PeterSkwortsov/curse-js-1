92d1bf8725cba643c4069827a14c863a0840e184
Peter <skwortsow.petia@yandex.ru>
2023-05-22 12:41:07 +0300

    Упражнение на bind

--- app.js
+++ app.js
@@ -1,34 +1,18 @@
 'use strict'
 
-const audi = {
-    make: 'Audo',
-    model: 'A3',
-    year: 2023,
-    dameges: [],
-    addDemeges (part, rate) {
-        console.log(`У авто ${this.model} ${this.make} ${this.year} добавлено повереждение: 
-        ${part} со степенью ${rate}`)
-        this.dameges.push({
-            part,
-            rate
-        })
+function removePassvord (reset) {
+    if (reset) {
+        this.password = undefined;
+    } else {
+        this.password = '1';
     }
-}
-audi.addDemeges ('капот', 2)
+} 
 
-const bmv = {
-    make: 'Bmv',
-    model: 'А5',
-    year: 2021,
-    dameges: [],
+const user = {
+    login: 'asd@sd.ru',
+    password: 12345
 };
 
-bmv.addDemeges = audi.addDemeges
-bmv.addDemeges ('Мотор', 4)
-
-const newFunction = audi.addDemeges;
-newFunction.call(bmv, 'колесо', 1)
-newFunction.call(audi, 'крыша', 5)
-
-newFunction.apply(audi, ['крыша', 5])
-newFunction.apply(bmv, ['кузов', 5])
+const resetUserParol = removePassvord.bind(user, true);
+resetUserParol()
+console.log(user)
