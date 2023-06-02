5f29e4f6dcb12138cb840de9ba4130054b8aa39d
Peter <skwortsow.petia@yandex.ru>
2023-06-02 11:13:47 +0300

    Суммировани в массиве

--- popitca2/app.js
+++ popitca2/app.js
@@ -1,13 +1,19 @@
 
 'use strict'
 
-const res = []
-for (let i = 10; i <= 30; i += 10) {
-  for (let j = 1; j <= 3; j++) {
-    res.push(i + j);
+
+let arr = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]];
+let res = 0;
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr[i].length; j++) {
+    for (let k = 0; k < arr[i][j].length; k++) {
+      res += arr[i][j][k];
+      //console.log([i] + [j] + [k]);
+    }
   }
 }
-console.log(res.join(' '))
+console.log(res)
+  
 
 
 
